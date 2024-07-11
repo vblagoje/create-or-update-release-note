02803efc49792c53a38e945de6e7c182583ee896
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-11 14:15:51 +0200

    Release new version

--- create-or-update-release-note.js
+++ create-or-update-release-note.js
@@ -3,63 +3,66 @@ const { execSync } = require('child_process');
 const core = require('@actions/core');
 const axios = require('axios');
 
-try {
-  const noteName = process.env.NOTE_NAME;
-  const noteContent = process.env.NOTE_CONTENT;
-  const branchName = process.env.GITHUB_HEAD_REF;
-  const baseBranchName = process.env.GITHUB_BASE_REF;
-  const repo = process.env.GITHUB_REPOSITORY;
-  const token = process.env.GITHUB_TOKEN;
+async function run() {
+  try {
+    const noteName = process.env.NOTE_NAME;
+    const noteContent = process.env.NOTE_CONTENT;
+    const branchName = process.env.GITHUB_HEAD_REF;
+    const baseBranchName = process.env.GITHUB_BASE_REF;
+    const repo = process.env.GITHUB_REPOSITORY;
+    const token = process.env.GITHUB_TOKEN;
 
-  if (!branchName || !baseBranchName || !repo || !token) {
-    throw new Error('GITHUB_HEAD_REF, GITHUB_BASE_REF, GITHUB_REPOSITORY, or GITHUB_TOKEN is not set');
-  }
-
-  const [owner, repoName] = repo.split('/');
-
-  // Use GitHub REST API to compare branches
-  const compareUrl = `https://api.github.com/repos/${owner}/${repoName}/compare/${baseBranchName}...${branchName}`;
-  const response = await axios.get(compareUrl, {
-    headers: { Authorization: `token ${token}` }
-  });
-
-  const files = response.data.files;
-  const noteAlreadyExists = files.some(file => file.filename.includes('releasenotes/notes') && file.filename.endsWith('.yaml'));
-
-  console.log(`Note already exists: ${noteAlreadyExists}`);
-  if (noteAlreadyExists) {
-    console.log("Existing note detected. Skipping further actions.");
-    return; // Exit the script without performing any further actions
-  }
-  let fileName;
-
-  // Checkout the branch if it exists
-  if (branchName) {
-    try {
-      execSync(`git checkout ${branchName}`);
-      console.log(`Checked out branch: ${branchName}`);
-    } catch (error) {
-      throw new Error(`Failed to checkout branch ${branchName}: ${error.message}`);
+    if (!branchName || !baseBranchName || !repo || !token) {
+      throw new Error('GITHUB_HEAD_REF, GITHUB_BASE_REF, GITHUB_REPOSITORY, or GITHUB_TOKEN is not set');
     }
+
+    const [owner, repoName] = repo.split('/');
+
+    // Use GitHub REST API to compare branches
+    const compareUrl = `https://api.github.com/repos/${owner}/${repoName}/compare/${baseBranchName}...${branchName}`;
+    const response = await axios.get(compareUrl, {
+      headers: { Authorization: `token ${token}` }
+    });
+
+    const files = response.data.files;
+    const noteAlreadyExists = files.some(file => file.filename.includes('releasenotes/notes') && file.filename.endsWith('.yaml'));
+
+    console.log(`Note already exists: ${noteAlreadyExists}`);
+    if (noteAlreadyExists) {
+      console.log("Existing note detected. Skipping further actions.");
+    } else {
+      let fileName;
+
+      // Checkout the branch if it exists
+      if (branchName) {
+        try {
+          execSync(`git checkout ${branchName}`);
+          console.log(`Checked out branch: ${branchName}`);
+        } catch (error) {
+          throw new Error(`Failed to checkout branch ${branchName}: ${error.message}`);
+        }
+      }
+
+      console.log("Creating new note.");
+      execSync(`reno new ${noteName}`);
+      fileName = fs.readdirSync('releasenotes/notes/').find(file => file.startsWith(noteName));
+      console.log(`Note created: ${fileName}`);
+
+      fs.writeFileSync(`releasenotes/notes/${fileName}`, noteContent + '\n'); // Newline at the end
+      console.log(`Content successfully written to releasenotes/notes/${fileName}`);
+
+      // Perhaps allow for custom user and email?
+      execSync('git config user.email "action@github.com"');
+      execSync('git config user.name "GitHub Action"');
+
+      // Do the git dance
+      execSync(`git add releasenotes/notes/${fileName}`);
+      execSync('git commit -m "Add release note"');
+      execSync(`git push origin HEAD:${branchName}`);
+    }
+  } catch (error) {
+    core.setFailed(`Action failed with error ${error}`);
   }
-
-  console.log("Creating new note.");
-  execSync(`reno new ${noteName}`);
-  fileName = fs.readdirSync('releasenotes/notes/').find(file => file.startsWith(noteName));
-  console.log(`Note created: ${fileName}`);
-
-  fs.writeFileSync(`releasenotes/notes/${fileName}`, noteContent + '\n'); // Newline at the end
-  console.log(`Content successfully written to releasenotes/notes/${fileName}`);
-
-  // Perhaps allow for custom user and email?
-  execSync('git config user.email "action@github.com"');
-  execSync('git config user.name "GitHub Action"');
-
-  // Do the git dance
-  execSync(`git add releasenotes/notes/${fileName}`);
-  execSync('git commit -m "Add release note"');
-  execSync(`git push origin HEAD:${branchName}`);
-
-} catch (error) {
-  core.setFailed(`Action failed with error ${error}`);
 }
+
+run();
--- dist/index.js
+++ dist/index.js
@@ -24705,6 +24705,14 @@ exports["default"] = _default;
 
 /***/ }),
 
+/***/ 3778:
+/***/ ((module) => {
+
+module.exports = eval("require")("axios");
+
+
+/***/ }),
+
 /***/ 9491:
 /***/ ((module) => {
 
@@ -26600,42 +26608,71 @@ var __webpack_exports__ = {};
 const fs = __nccwpck_require__(7147);
 const { execSync } = __nccwpck_require__(2081);
 const core = __nccwpck_require__(2186);
+const axios = __nccwpck_require__(3778);
 
-try {
-  const noteName = process.env.NOTE_NAME;
-  const noteContent = process.env.NOTE_CONTENT;
-  let fileName;
+async function run() {
+  try {
+    const noteName = process.env.NOTE_NAME;
+    const noteContent = process.env.NOTE_CONTENT;
+    const branchName = process.env.GITHUB_HEAD_REF;
+    const baseBranchName = process.env.GITHUB_BASE_REF;
+    const repo = process.env.GITHUB_REPOSITORY;
+    const token = process.env.GITHUB_TOKEN;
 
-  const branchName = process.env.GITHUB_HEAD_REF;
-  if (branchName) {
-    execSync(`git checkout ${branchName}`);
+    if (!branchName || !baseBranchName || !repo || !token) {
+      throw new Error('GITHUB_HEAD_REF, GITHUB_BASE_REF, GITHUB_REPOSITORY, or GITHUB_TOKEN is not set');
+    }
+
+    const [owner, repoName] = repo.split('/');
+
+    // Use GitHub REST API to compare branches
+    const compareUrl = `https://api.github.com/repos/${owner}/${repoName}/compare/${baseBranchName}...${branchName}`;
+    const response = await axios.get(compareUrl, {
+      headers: { Authorization: `token ${token}` }
+    });
+
+    const files = response.data.files;
+    const noteAlreadyExists = files.some(file => file.filename.includes('releasenotes/notes') && file.filename.endsWith('.yaml'));
+
+    console.log(`Note already exists: ${noteAlreadyExists}`);
+    if (noteAlreadyExists) {
+      console.log("Existing note detected. Skipping further actions.");
+    } else {
+      let fileName;
+
+      // Checkout the branch if it exists
+      if (branchName) {
+        try {
+          execSync(`git checkout ${branchName}`);
+          console.log(`Checked out branch: ${branchName}`);
+        } catch (error) {
+          throw new Error(`Failed to checkout branch ${branchName}: ${error.message}`);
+        }
+      }
+
+      console.log("Creating new note.");
+      execSync(`reno new ${noteName}`);
+      fileName = fs.readdirSync('releasenotes/notes/').find(file => file.startsWith(noteName));
+      console.log(`Note created: ${fileName}`);
+
+      fs.writeFileSync(`releasenotes/notes/${fileName}`, noteContent + '\n'); // Newline at the end
+      console.log(`Content successfully written to releasenotes/notes/${fileName}`);
+
+      // Perhaps allow for custom user and email?
+      execSync('git config user.email "action@github.com"');
+      execSync('git config user.name "GitHub Action"');
+
+      // Do the git dance
+      execSync(`git add releasenotes/notes/${fileName}`);
+      execSync('git commit -m "Add release note"');
+      execSync(`git push origin HEAD:${branchName}`);
+    }
+  } catch (error) {
+    core.setFailed(`Action failed with error ${error}`);
   }
-
-  if (fs.existsSync(`releasenotes/notes/${noteName}.yaml`)) {
-    console.log("Updating existing note.");
-    fileName = `releasenotes/notes/${noteName}.yaml`;
-  } else {
-    console.log("Creating new note.");
-    execSync(`reno new ${noteName}`);
-    fileName = fs.readdirSync('releasenotes/notes/').find(file => file.startsWith(noteName));
-    console.log(`Note created: ${fileName}`);
-  }
-
-  fs.writeFileSync(`releasenotes/notes/${fileName}`, noteContent + '\n'); // Newline at the end
-  console.log(`Content successfully written to releasenotes/notes/${fileName}`);
-
-  // Perhaps allow for custom user and email?
-  execSync('git config user.email "action@github.com"');
-  execSync('git config user.name "GitHub Action"');
-
-  // Do the git dance
-  execSync(`git add releasenotes/notes/${fileName}`);
-  execSync('git commit -m "Add release note"');
-  execSync(`git push origin HEAD:${branchName}`);
-
-} catch (error) {
-  core.setFailed(`Action failed with error ${error}`);
 }
+
+run();
 
 })();
 
