2d4eac0839616eeeba65a9e59f1021150efa8afe
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-11 13:34:31 +0200

    Use axios

--- create-or-update-release-note.js
+++ create-or-update-release-note.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 const core = require('@actions/core');
+const axios = require('axios');
 
 try {
   const noteName = process.env.NOTE_NAME;
